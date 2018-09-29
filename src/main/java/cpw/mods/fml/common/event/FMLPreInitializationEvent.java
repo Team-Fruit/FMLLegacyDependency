5aa0b5a5368c95f8846069741840b7275725f388
Kamesuta <kamesuta@gmail.com>
2018-09-29 11:58:18 +0900

    add method to preinitevent

--- src/main/java/cpw/mods/fml/common/event/FMLPreInitializationEvent.java
+++ src/main/java/cpw/mods/fml/common/event/FMLPreInitializationEvent.java
@@ -1,4 +1,13 @@
 package cpw.mods.fml.common.event;
 
+import java.io.File;
+
+import org.apache.logging.log4j.Logger;
+
 public interface FMLPreInitializationEvent extends FMLStateEvent {
+	public File getSourceFile();
+
+	public File getSuggestedConfigurationFile();
+
+	public Logger getModLog();
 }
