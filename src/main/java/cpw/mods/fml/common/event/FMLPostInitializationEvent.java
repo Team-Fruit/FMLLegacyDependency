23842c61d7c54b57d54e2a8cb1860f24e7c62172
Kamesuta <kamesuta@gmail.com>
2018-11-04 06:06:45 +0900

    add method

--- src/main/java/cpw/mods/fml/common/event/FMLPostInitializationEvent.java
+++ src/main/java/cpw/mods/fml/common/event/FMLPostInitializationEvent.java
@@ -1,4 +1,7 @@
 package cpw.mods.fml.common.event;
 
+import java.util.Optional;
+
 public abstract class FMLPostInitializationEvent extends FMLStateEvent {
+	public abstract Optional<?> buildSoftDependProxy(String modId, String className, Object... arguments);
 }
--- src/main/java/cpw/mods/fml/common/event/FMLPreInitializationEvent.java
+++ src/main/java/cpw/mods/fml/common/event/FMLPreInitializationEvent.java
@@ -1,13 +1,22 @@
 package cpw.mods.fml.common.event;
 
 import java.io.File;
+import java.util.Properties;
 
 import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.ModMetadata;
 
 public abstract class FMLPreInitializationEvent extends FMLStateEvent {
 	public abstract File getSourceFile();
 
+	public abstract ModMetadata getModMetadata();
+
+	public abstract File getModConfigurationDirectory();
+
 	public abstract File getSuggestedConfigurationFile();
+
+	public abstract Properties getVersionProperties();
 
 	public abstract Logger getModLog();
 }
